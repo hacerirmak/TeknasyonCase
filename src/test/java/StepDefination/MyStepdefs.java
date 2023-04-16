539633de6beec8e750aba938d2cf30dac2c8886e
hacerirmak <hcrirmak@gmail.com>
2023-04-16 11:59:39 +0300

    Create Project

---
+++ src/test/java/StepDefination/MyStepdefs.java
@@ -0,0 +1,23 @@
+package StepDefination;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+    @Given("Haedar alanındaki başlık kontrol edilir.")
+    public void haedarAlanındakiBaşlıkKontrolEdilir() {
+    }
+
+    @And("Devam butonuna tıklanır.")
+    public void devamButonunaTıklanır() {
+    }
+
+    @And("Pop up kontrol edilir.")
+    public void popUpKontrolEdilir() {
+    }
+
+    @Then("Ok butonuna tıklanır.")
+    public void okButonunaTıklanır() {
+    }
+}
---
+++ src/test/java/teknasyonotomasyon/Scenario.feature
@@ -0,0 +1,10 @@
+Feature: Uygulama izni verilerek yeni sekmelerin acilmasi
+
+  Scenario: 1- Uygulama izni verilmesi
+    Given Haedar alanındaki başlık kontrol edilir.
+    And Devam butonuna tıklanır.
+    And Pop up kontrol edilir.
+    Then Ok butonuna tıklanır.
+
+    Scenario:
+
